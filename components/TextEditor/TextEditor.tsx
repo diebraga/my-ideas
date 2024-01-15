2a43557aadfadb9526d8009c6cc9b6791b1bc5e2
Diego Braga <diebraga0055@gmail.com>
2024-01-15 22:05:06 +0000

    sep toolbar

--- components/TextEditor/TextEditor.tsx
+++ components/TextEditor/TextEditor.tsx
@@ -1,171 +1,15 @@
-import { useCallback, useEffect, useRef, useState } from "react";
-import { IconButton } from "@material-tailwind/react";
-import {
-  $getSelection,
-  $isRangeSelection,
-  FORMAT_TEXT_COMMAND,
-  SELECTION_CHANGE_COMMAND,
-} from "lexical";
-import { $isListNode } from "@lexical/list";
-import { QuoteNode, HeadingNode, $isHeadingNode } from "@lexical/rich-text";
+import { useState } from "react";
+import { QuoteNode, HeadingNode } from "@lexical/rich-text";
 import { ListItemNode, ListNode } from "@lexical/list";
 import { AutoLinkNode, LinkNode } from "@lexical/link";
 import { CodeHighlightNode, CodeNode } from "@lexical/code";
 import { LinkPlugin } from "@lexical/react/LexicalLinkPlugin";
 import { ListPlugin } from "@lexical/react/LexicalListPlugin";
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import { $getNearestNodeOfType, mergeRegister } from "@lexical/utils";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-
-const LowPriority = 1;
-
-function ToolbarPlugin() {
-  const [editor] = useLexicalComposerContext();
-  const toolbarRef = useRef(null);
-  const [isBold, setIsBold] = useState(false);
-  const [isItalic, setIsItalic] = useState(false);
-  const [isCode, setIsCode] = useState(false);
-
-  const updateToolbar = useCallback(() => {
-    const selection = $getSelection();
-    if ($isRangeSelection(selection)) {
-      const anchorNode = selection.anchor.getNode();
-      const element =
-        anchorNode.getKey() === "root"
-          ? anchorNode
-          : anchorNode.getTopLevelElementOrThrow();
-      const elementKey = element.getKey();
-      const elementDOM = editor.getElementByKey(elementKey);
-      if (elementDOM !== null) {
-        if ($isListNode(element)) {
-          const parentList = $getNearestNodeOfType(anchorNode, ListNode);
-          const type = parentList ? parentList.getTag() : element.getTag();
-        } else {
-          const type = $isHeadingNode(element)
-            ? element.getTag()
-            : element.getType();
-        }
-      }
-      // Update text format
-      setIsBold(selection.hasFormat("bold"));
-      setIsItalic(selection.hasFormat("italic"));
-      setIsCode(selection.hasFormat("code"));
-    }
-  }, [editor]);
-
-  useEffect(() => {
-    return mergeRegister(
-      editor.registerUpdateListener(({ editorState }) => {
-        editorState.read(() => {
-          updateToolbar();
-        });
-      }),
-      editor.registerCommand(
-        SELECTION_CHANGE_COMMAND,
-        (_payload) => {
-          updateToolbar();
-          return false;
-        },
-        LowPriority
-      )
-    );
-  }, [editor, updateToolbar]);
-
-  return (
-    <div
-      className="m-1 flex items-center gap-0.5 rounded-lg bg-gray-100 p-1"
-      ref={toolbarRef}
-    >
-      <IconButton
-        variant={isBold ? "filled" : "text"}
-        onClick={() => {
-          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "bold");
-        }}
-        aria-label="Format Bold"
-      >
-        <svg
-          strokeWidth="1.5"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          color="currentColor"
-          className="h-5 w-5"
-        >
-          <path
-            d="M12 11.6667H8M12 11.6667C12 11.6667 15.3333 11.6667 15.3333 8.33333C15.3333 5.00002 12 5 12 5C12 5 12 5 12 5H8.6C8.26863 5 8 5.26863 8 5.6V11.6667M12 11.6667C12 11.6667 16 11.6667 16 15.3333C16 19 12 19 12 19C12 19 12 19 12 19H8.6C8.26863 19 8 18.7314 8 18.4V11.6667"
-            stroke="currentColor"
-            strokeWidth="1.5"
-          ></path>
-        </svg>
-      </IconButton>
-      <IconButton
-        variant={isItalic ? "filled" : "text"}
-        onClick={() => {
-          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "italic");
-        }}
-        aria-label="Format Italics"
-      >
-        <svg
-          strokeWidth="1.5"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          color="currentColor"
-          className="h-5 w-5"
-        >
-          <path
-            d="M11 5L14 5M17 5L14 5M14 5L10 19M10 19L7 19M10 19L13 19"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          ></path>
-        </svg>
-      </IconButton>
-      <IconButton
-        variant={isCode ? "filled" : "text"}
-        onClick={() => {
-          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "code");
-        }}
-        aria-label="Insert Code"
-      >
-        <svg
-          strokeWidth="1.5"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          color="currentColor"
-          className="h-5 w-5"
-        >
-          <path
-            d="M13.5 6L10 18.5"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          ></path>
-          <path
-            d="M6.5 8.5L3 12L6.5 15.5"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          ></path>
-          <path
-            d="M17.5 8.5L21 12L17.5 15.5"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          ></path>
-        </svg>
-      </IconButton>
-    </div>
-  );
-}
+import { ToolbarPlugin } from "../ToolbarPlugin/ToolbarPlugin";
 
 const editorConfig = {
   namespace: "MyEditor",
@@ -184,15 +28,33 @@ const editorConfig = {
   ],
 };
 
-export function TextEditor() {
+export const TextEditor = () => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
   return (
     <LexicalComposer initialConfig={editorConfig}>
-      <div className="relative mx-auto overflow-hidden my-5 w-full rounded-xl border border-gray-400 text-left font-normal leading-5 text-gray-900">
+      <div
+        className={`relative mx-auto overflow-hidden my-5 w-full rounded-xl ${
+          isFocused ? "border-black border-2" : "border-gray-400 border"
+        } text-left font-normal leading-5 text-gray-900`}
+      >
         <ToolbarPlugin />
         <div className="relative rounded-b-lg border-opacity-5 bg-white">
           <RichTextPlugin
             contentEditable={
-              <ContentEditable className="lexical min-h-[280px] resize-none px-2.5 py-4 text-base caret-gray-900 outline-none" />
+              <ContentEditable
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                className="lexical min-h-[280px] resize-none px-2.5 py-4 text-base caret-gray-900 outline-none"
+              />
             }
             placeholder={
               <div className="pointer-events-none absolute left-2.5 top-4 inline-block select-none overflow-hidden text-base font-normal text-gray-400">
@@ -209,4 +71,4 @@ export function TextEditor() {
       </div>
     </LexicalComposer>
   );
-}
+};
